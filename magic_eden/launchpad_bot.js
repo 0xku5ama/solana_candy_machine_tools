becd64b71d1dafac39002786fe1588ff467cb029
0xku5ama <97934153+0xku5ama@users.noreply.github.com>
2022-01-18 13:11:25 +0800

    Create launchpad_bot.js

---
+++ magic_eden/launchpad_bot.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Magic Eden launchpad bot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  This is a tamper monkey script to bot Magic Eden's launchpad.
+// @author       0xku5ama
+// @match        https://magiceden.io/launchpad/*
+// @icon         https://www.google.com/s2/favicons?domain=google.com
+// @grant        none
+// ==/UserScript==
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function bot() {
+    while (true) {
+        try {
+            let button = document.querySelector('.me-btn'); // Magic eden css selector to select mint button
+            button.click();
+            await sleep(100); // Spam every 0.1s, spamming too fast can cause browser to hang
+            console.log("Mint button clicked");
+        } catch (err) {
+            console.log("Button not ready");
+            await sleep(5000);
+        }
+    }
+}
+
+(function() {
+    'use strict';
+    // Your code here...
+    bot();
+})();
